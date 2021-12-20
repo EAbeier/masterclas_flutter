74bd19f53aa59fdf76efdf866dd1751b51613a58
EAbeier <emersonabeier@hotmail.com>
2021-12-20 15:28:03 -0300

    recriação expansiontile com Animações implícitas

--- Animacoes/recriacao_expansion_tile/lib/lista.dart
+++ Animacoes/recriacao_expansion_tile/lib/lista.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Lista extends StatefulWidget {
+class CustomExpansionTile extends StatefulWidget {
+  final String tileText;
+  const CustomExpansionTile({required this.tileText});
+
   @override
-  State<Lista> createState() => _ListaState();
+  State<CustomExpansionTile> createState() => _CustomExpansionTileState();
 }
 
-class _ListaState extends State<Lista> {
+class _CustomExpansionTileState extends State<CustomExpansionTile> {
   bool isExpanded = false;
 
   @override
@@ -17,16 +20,33 @@ class _ListaState extends State<Lista> {
         });
       },
       child: Card(
-        child: ListTile(
-          title: Text("TEXTO"),
-          trailing: AnimatedRotation(
-            duration: Duration(seconds: 1),
-            turns: isExpanded ? 1.0 / 2.0 : 0.0,
-            child: Icon(
-              Icons.expand_more,
-              color: isExpanded ? Colors.blue : null,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(widget.tileText),
+              trailing: AnimatedRotation(
+                duration: Duration(seconds: 1),
+                turns: isExpanded ? 1.0 / 2.0 : 0.0,
+                child: Icon(
+                  Icons.expand_more,
+                  color: isExpanded ? Colors.blue : null,
+                ),
+              ),
             ),
-          ),
+            ClipRect(
+              child: AnimatedAlign(
+                duration: Duration(seconds: 1),
+                alignment: Alignment.center,
+                heightFactor: isExpanded ? 1 : 0,
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                      ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                      ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' +
+                      ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
